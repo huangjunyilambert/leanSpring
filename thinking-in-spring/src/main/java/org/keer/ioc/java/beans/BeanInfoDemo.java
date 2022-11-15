47efb87416b92bb1259bf5dbc8cc4ed0932dc814
huangjunyi <huangjy@tuya.com>
2022-11-15 21:37:47 +0800

    获取bean属性

---
+++ thinking-in-spring/src/main/java/org/keer/ioc/java/beans/BeanInfoDemo.java
@@ -0,0 +1,19 @@
+package org.keer.ioc.java.beans;
+
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.util.stream.Stream;
+
+/**
+ * @author huangjunyi
+ * @date 2022/11/15 21:12:53
+ */
+public class BeanInfoDemo {
+    public static void main(String[] args) throws IntrospectionException {
+        BeanInfo beanInfo = Introspector.getBeanInfo(Person.class, Object.class);
+        Stream.of(beanInfo.getPropertyDescriptors()).forEach(propertyDescriptor -> {
+            System.out.println(propertyDescriptor);
+        });
+    }
+}
---
+++ thinking-in-spring/src/main/java/org/keer/ioc/java/beans/Person.java
@@ -0,0 +1,26 @@
+package org.keer.ioc.java.beans;
+
+/**
+ * @author huangjunyi
+ * @date 2022/11/15 21:10:50
+ */
+public class Person {
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
