fb7990f1a9f949b3bbaa361a397bf8a8390b57b5
huangjunyi <huangjy@tuya.com>
2022-11-16 22:00:17 +0800

    自动注入

--- thinking-in-spring/ioc-container-overview/src/main/java/org/keer/think/in/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
+++ thinking-in-spring/ioc-container-overview/src/main/java/org/keer/think/in/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
@@ -2,6 +2,7 @@ package org.keer.think.in.spring.ioc.overview.dependency.injection;
 
 import org.keer.think.in.spring.ioc.overview.dependency.repository.UserRepository;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -15,6 +16,10 @@ public class DependencyInjectionDemo {
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
         UserRepository userRepository = (UserRepository) beanFactory.getBean("userRepository");
         System.out.println(userRepository.getUsers());
+        ObjectFactory objectFactory = userRepository.getObjectFactory();
+        System.out.println(objectFactory.getObject());
+        System.out.println(objectFactory.getObject() == beanFactory);
+
 
     }
 
--- thinking-in-spring/ioc-container-overview/src/main/java/org/keer/think/in/spring/ioc/overview/dependency/repository/UserRepository.java
+++ thinking-in-spring/ioc-container-overview/src/main/java/org/keer/think/in/spring/ioc/overview/dependency/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package org.keer.think.in.spring.ioc.overview.dependency.repository;
 
 import org.keer.think.in.spring.ioc.overview.dependency.domain.User;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.context.ApplicationContext;
 
 import java.util.Collection;
 
@@ -11,11 +14,32 @@ import java.util.Collection;
 public class UserRepository {
     private Collection<User> users;
 
+    private BeanFactory beanFactory;
+
+    private ObjectFactory<ApplicationContext> objectFactory;
+
     public Collection<User> getUsers() {
         return users;
     }
 
     public void setUsers(Collection<User> users) {
         this.users = users;
+    }
+
+    public BeanFactory getBeanFactory() {
+        return beanFactory;
+    }
+
+    public void setBeanFactory(BeanFactory beanFactory) {
+        this.beanFactory = beanFactory;
+    }
+
+    public ObjectFactory<ApplicationContext> getObjectFactory() {
+        return objectFactory;
+    }
+
+    public void setObjectFactory(
+            ObjectFactory<ApplicationContext> objectFactory) {
+        this.objectFactory = objectFactory;
     }
 }
