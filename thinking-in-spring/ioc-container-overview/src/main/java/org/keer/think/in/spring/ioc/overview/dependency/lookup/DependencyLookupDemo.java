eb36f85bd44fbd2eb552beb40ad8b19f7fb96a15
huangjunyi <huangjy@tuya.com>
2022-11-16 18:18:19 +0800

    根据bean 名称查找

--- thinking-in-spring/ioc-container-overview/src/main/java/org/keer/think/in/spring/ioc/overview/dependency/lookup/DependencyLookupDemo.java
+++ thinking-in-spring/ioc-container-overview/src/main/java/org/keer/think/in/spring/ioc/overview/dependency/lookup/DependencyLookupDemo.java
@@ -2,6 +2,7 @@ package org.keer.think.in.spring.ioc.overview.dependency.lookup;
 
 import org.keer.think.in.spring.ioc.overview.dependency.domain.User;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -13,8 +14,21 @@ public class DependencyLookupDemo {
         //配置 xml文件
         //启动 spring 应用上下文
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-lookup-context.xml");
-        User user = (User) beanFactory.getBean("user");
-        System.out.println(user);
+        lookupInRealTime(beanFactory);
+        lookupInLazy(beanFactory);
 
     }
+
+    private static void lookupInRealTime(BeanFactory beanFactory) {
+        User user = (User) beanFactory.getBean("user");
+        System.out.println("实时查找：" + user);
+    }
+
+    private static void lookupInLazy(BeanFactory beanFactory) {
+        ObjectFactory<User> objectFactory = (ObjectFactory<User>) beanFactory.getBean("objectFactory");
+        User user = objectFactory.getObject();
+        System.out.println("延迟查找：" + user);
+
+    }
+
 }
